92639739680fa52466295a6beed0487734007533
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-12-13 17:55:14 -0600

    added sentry config

--- Spns/settings.py
+++ Spns/settings.py
@@ -13,6 +13,23 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://30148fcfad974b9f804d1b8667b2ba70@o930234.ingest.sentry.io/6105858",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
